850250de22c803d595b24f2bcc0e1f286f3ca39b
Rishab59 <hrishab59@gmail.com>
2025-03-17 08:07:25 +0530

    Made Database.java

---
+++ src/Controller/AddNewAdmin.java
@@ -0,0 +1,16 @@
+package Controller;
+
+import java.util.Scanner;
+
+import Model.Database;
+import Model.Operation;
+import Model.User;
+
+public class AddNewAdmin implements Operation {
+
+	@Override
+	public void operation(Database database, Scanner sc, User user) {
+		
+	}
+	
+}
